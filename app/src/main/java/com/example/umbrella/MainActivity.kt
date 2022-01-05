567480c5c05cf7965b0e14ff4786f403718dd591
Evan-Coulter <ercoulte@sfu.ca>
2022-01-04 18:22:37 -0800

    First UI Changes,
    Added basic scaffold layout for future use.

--- app/src/main/java/com/example/umbrella/MainActivity.kt
+++ app/src/main/java/com/example/umbrella/MainActivity.kt
@@ -1,38 +1,64 @@
 package com.example.umbrella
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.umbrella.ui.theme.UmbrellaTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UmbrellaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            DisplayMainContent()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DisplayMainContent() {
+    UmbrellaTheme{
+        val scaffoldState = rememberScaffoldState()
+        val scaffoldCoroutineScope = rememberCoroutineScope()
+        Surface(color = MaterialTheme.colors.background) {
+            Scaffold(
+                scaffoldState = scaffoldState,
+                floatingActionButton = {
+                    ExtendedFloatingActionButton(text = {Text(text="Add City")}, modifier = Modifier.padding(start = 24.dp), onClick= {
+                        scaffoldCoroutineScope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("City Added")
+                        }
+                    })
+               },
+                content = {
+                    Column{
+                        Text(text="Weather Graphic Here")
+                        Text(text="Cities Observed Here")
+                    }
+                }
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    UmbrellaTheme {
-        Greeting("Android")
-    }
+fun PreviewMainContent() {
+    DisplayMainContent()
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewMainContentDark() {
+    DisplayMainContent()
 }
